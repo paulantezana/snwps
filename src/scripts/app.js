6fbb03bb35583ace52b6bb106a2cec1d2fe3a842
paul antezana <yoel.antezana@gmail.com>
2019-03-28 18:12:01 -0500

    new updates

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,10 @@
 import SNMenu from './components/menu';
 
 // Primary Menu
-SNMenu('PrimaryMenu','PrimaryMenu-toggle','Site','PrimaryMenu-is-show');
-
-// Top Menu
-SNMenu('TopMenu','TopMenu-toggle','TopMenu-container','TopMenu-is-show');
+SNMenu({
+    menuId: 'PrimaryMenu',
+    toggleButtonID: 'PrimaryMenu-toggle',
+    toggleClass: 'PrimaryMenu-is-show',
+    parentClose: true,
+    menuCloseID: 'PrimaryMenu-close',
+});
--- src/scripts/components/menu.js
+++ src/scripts/components/menu.js
@@ -1,46 +1,76 @@
-let SNMenu = (menuId, toggleButtonID, contextId='Site', toggleClass = "Menu-is-show") => {
+let SNMenu = ({
+    menuId = "Menu",
+    toggleButtonID = "Menu-toggle",
+    contextId = "Site",
+    toggleClass = "Menu-is-show",
+    parentClose = false,
+    menuCloseID = ''
+}) => {
     // Get Menu
     let menuEl = document.getElementById(menuId);
-    if(!menuEl) return;
+    if (!menuEl) return;
 
     // Sub menus dinamicos
-    let items = menuEl.querySelectorAll('li'); // select all items
-    for (let ele of items){
-        if (ele.childElementCount === 2){ // if submenu
+    let items = menuEl.querySelectorAll("li"); // select all items
+    for (let ele of items) {
+        if (ele.childElementCount === 2) {
+            // if submenu
             let toggle = ele.firstElementChild; // First Element
-            let content = ele.lastElementChild; // Second Element  
+            let content = ele.lastElementChild; // Second Element
 
             // Creando un nuevo elemento e insertando justo despues del enlace
-            let iconToggleEle = document.createElement('i');
-            iconToggleEle.classList.add('icon-down');
-            iconToggleEle.classList.add('Toggle');
+            let iconToggleEle = document.createElement("i");
+            iconToggleEle.classList.add("icon-down");
+            iconToggleEle.classList.add("Toggle");
             toggle.appendChild(iconToggleEle);
+            toggle.classList.add('is-toggle')
 
             // Escuchando los eventos click
-            iconToggleEle.addEventListener('click',(e)=>{
+            iconToggleEle.addEventListener("click", e => {
                 e.preventDefault();
-                iconToggleEle.classList.toggle('icon-up'); // add Icon up
-                content.classList.toggle('is-show');   // add class show menu
+                iconToggleEle.classList.toggle("icon-up"); // add Icon up
+                content.classList.toggle("is-show"); // add class show menu
             });
         }
     }
 
     // Agregar la clase active en los enlaces
     const links = [...menuEl.querySelectorAll('a')];
-    if(links){
-        links.map( link => {
+    if (links) {
+        links.map(link => {
             const url = document.location.href;
-            if(link.href === url || link.href === url.slice(0,-1)) link.classList.add('is-active');
-        })
+            if (link.href === url || link.href === url.slice(0, -1))
+                link.parentNode.classList.add('is-active');
+        });
     }
 
     // Toggle Menu
     let button = document.getElementById(toggleButtonID);
     let context = document.getElementById(contextId);
-    if(button && context){
-        button.addEventListener('click',()=>{
+    if (button && context) {
+        button.addEventListener("click", () => {
             context.classList.toggle(toggleClass);
         });
+    }
+
+    // Menu close quitar la clase
+    if(menuCloseID!== ''){
+        let menuClose = document.getElementById(menuCloseID);
+        if (menuClose) {
+            menuClose.addEventListener("click", () => {
+                context.classList.remove(toggleClass);
+            });
+        }
+    }
+
+    // Hide menu by parent
+    if(parentClose){
+        menuEl.parentNode.addEventListener("click", e => {
+            context.classList.remove(toggleClass);
+        });
+        menuEl.addEventListener('click',e=>{
+            e.stopPropagation();
+        })
     }
 };
 
